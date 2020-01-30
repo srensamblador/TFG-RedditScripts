9ceb45a60583e0c71b7b86140e108b8b7f98bb8a
Samuel Cifuentes <uo238524@uniovi.es>
2020-01-30 17:36:56 +0100

    Se indexan los documentos en Elastic

---
+++ elastic_indexers.py
@@ -0,0 +1,131 @@
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+
+
+class Indexer:
+    def __init__(self, connection, index_name):
+        self.es = connection
+        self.index_name = index_name
+
+    def create_index(self):
+        arguments = {
+            "settings": {
+                "index": {
+                    "analysis": {
+                        "analyzer": {
+                            "default": {
+                                "tokenizer": "standard",
+                                "filter": ["lowercase", "filter_stopword"]
+                            }
+                        },
+                        "filter": {
+                            "filter_stopword": {
+                                "type": "stop",
+                                "stopwords": ["a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "another", "any", "are", "aren't", "as", "at", "back", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "even", "ever", "every", "few", "first", "five", "for", "four", "from", "further", "get", "go", "goes", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "high", "him", "himself", "his", "how", "how's", "however", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "just", "least", "less", "let's", "like", "long", "made", "make", "many", "me", "more", "most", "mustn't", "my", "myself", "never", "new", "no", "nor", "not", "now", "of", "off", "old", "on", "once", "one", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "put", "said", "same", "say", "says", "second", "see", "seen", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "since", "so", "some", "still", "such", "take", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "three", "through", "to", "too", "two", "under", "until", "up", "very", "was", "wasn't", "way", "we", "we'd", "we'll", "we're", "we've", "well", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "whether", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        self.es.indices.create(index=self.index_name, body=arguments)
+
+        # Para poder utilizar agregaciones sobre los campos title y selftext es necesario activar fielddata
+        arguments = {
+            "properties": {
+                "title": {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "default"
+                },
+                "selftext": {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "default"
+                },
+                "subreddit": {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "default"
+                }
+            }
+        }
+
+        self.es.indices.put_mapping(
+            index=self.index_name, doc_type="post", body=arguments,  include_type_name=True)
+
+    def index_documents(self, documents):
+        toIndex = []
+
+        for document in documents:
+            ident = document["id"]
+
+            document["_index"] = self.index_name
+            document["_type"] = "post"
+            # Genera una explosión de campos en otro caso
+            document["media_metadata"] = None
+            document["_id"] = ident
+
+            toIndex.append(document)
+
+        helpers.bulk(self.es, toIndex, chunk_size=len(
+            toIndex), request_timeout=200)
+
+    def index_exists(self):
+        return self.es.indices.exists(index=self.index_name)
+
+
+class NgramIndexer(Indexer):
+    def create_index(self):
+        arguments = {
+            "settings": {
+                "index": {
+                    "analysis": {
+                        "analyzer": {
+                            "default": {
+                                "tokenizer": "standard",
+                                "filter": ["lowercase", "filter_stopwords"]
+                            },
+                            "analyzer_shingle": {
+                                "tokenizer": "standard",
+                                "filter": ["lowercase", "filter_stopwords", "filter_shingle"]
+                            }
+                        },
+                        "filter": {
+                            "filter_stopwords": {
+                                "type": "stop",
+                                "stopwords": ["a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "another", "any", "are", "aren't", "as", "at", "back", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "even", "ever", "every", "few", "first", "five", "for", "four", "from", "further", "get", "go", "goes", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "high", "him", "himself", "his", "how", "how's", "however", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "just", "least", "less", "let's", "like", "long", "made", "make", "many", "me", "more", "most", "mustn't", "my", "myself", "never", "new", "no", "nor", "not", "now", "of", "off", "old", "on", "once", "one", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "put", "said", "same", "say", "says", "second", "see", "seen", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "since", "so", "some", "still", "such", "take", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "three", "through", "to", "too", "two", "under", "until", "up", "very", "was", "wasn't", "way", "we", "we'd", "we'll", "we're", "we've", "well", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "whether", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+                            },
+                            "filter_shingle": {
+                                "type": "shingle",
+                                "max_shingle_size": 2,
+                                "min_shingle_size": 2,
+                                "output_unigrams": "false"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        self.es.indices.create(index=self.index_name, body=arguments)
+        arguments = {
+            "properties": {
+                "title": {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "analyzer_shingle"
+                },
+                "selftext": {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "analyzer_shingle"
+                },
+                "subreddit": {
+                    "type": "text",
+                    "fielddata": "true",
+                    "analyzer": "default"
+                }
+            }
+        }
+        self.es.indices.put_mapping(
+            index=self.index_name, doc_type="post", body=arguments,  include_type_name=True)
--- main.py
+++ main.py
@@ -2,24 +2,38 @@ from psaw import PushshiftAPI
 import argparse
 from datetime import datetime
 import json
-import os.path
-import time
+from elasticsearch import Elasticsearch
+from elastic_indexers import Indexer, NgramIndexer
 
 api = PushshiftAPI()
 
 def main(args):
+    # Establece la conexión a Elastic
+    global es
+    es = Elasticsearch()
+
+    # Crea el .json de backup donde se volcarán los post
     global filename
     filename = "dumps/" + args.query.replace(" " ,"") + "-Dump.json"
-
     with open(filename, "w") as f:
         f.write("{\"data\": [")
 
-    query_API(args.query, args.scale)
+    query_API(args.query, args.scale, cache_size=3000)
 
     with open(filename, "a") as f:
         f.write("]}")
 
-def query_API(query, scale):
+def query_API(query, scale, cache_size = 3000):
+    '''
+        Se utiliza psaw (https://github.com/dmarx/psaw) para consumir la API de Pushshift y extraer submissions. 
+        A cada submission se le añaden tres campos: la frase con la que se obtuvo, la escala a la que pertenece la frase y
+        un booleano para indicar que el post dio positivo en una escala.
+
+        :param query: frase a consultar
+        :param scale: escala a la que pertenece la frase
+        :param cache_size: cada cuantos post se realiza una escritura, opcional. Por defecto, 10000
+    '''
+
     gen = api.search_submissions(q=query)
     cache = []
     numIter = 0
@@ -28,13 +42,9 @@ def query_API(query, scale):
         if numIter >= 4:
             break
 
-        submission = c.d_
-        submission["query"] = query
-        submission["scale"] = scale
-        submission["lonely"] = True
-        cache.append(submission)
+        cache.append(c.d_)
 
-        if len(cache) == 3000:
+        if len(cache) == cache_size:
             print(".", end="")
             
             dump_to_file(cache, numIter == 0)
@@ -44,7 +54,9 @@ def query_API(query, scale):
             numIter += 1
 
 def dump_to_file(results, initial_dump):
-    start_time = time.time()
+    '''
+        Vuelca una lista de submissions a un fichero .json
+    '''
 
     if initial_dump:
         delimiter = ""
@@ -54,10 +66,14 @@ def dump_to_file(results, initial_dump):
     with open(filename, "a") as f:
         f.write(delimiter + json.dumps(results).strip("[").strip("]"))
 
-    print(time.time() - start_time)
-
 def elastic_index(results):
-    pass
+    indexers = [Indexer(es, "reddit-loneliness"), NgramIndexer(es, "reddit-loneliness-ngram")]
+    for indexer in indexers:
+        if not indexer.index_exists():
+            print("Creado índice: " + indexer.index_name)
+            indexer.create_index()
+        
+        indexer.index_documents(results)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Script para la extracción de submissions de Reddit a través de pushshift.io")
