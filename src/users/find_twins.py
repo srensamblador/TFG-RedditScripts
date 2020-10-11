1b4a0e9e7cefce1b8153769ce27da71f3d34413e
Samuel Cifuentes García <uo238524@uniovi.es>
2020-10-11 21:58:57 +0200

    Intervalos de tiempo y consulta iterativa
    Se calculan varios intervalos de tiempo para extraer posibles candidatos
    y se realizan consultas en bucle de intervalo más restrictivo a menos
    restrictivo hasta alcanzar un mínimo de usuarios o acabar todos los
    intervalos

--- src/users/find_twins.py
+++ src/users/find_twins.py
@@ -3,12 +3,15 @@ from elasticsearch import Elasticsearch, helpers
 from psaw import PushshiftAPI
 from datetime import date
 from datetime import datetime as dt
+from datetime import timedelta
 import progressbar as pb
 import pprint as pp
 import pickle
 
+import requests
 
-MAX_USERS = 5000
+
+MAX_USERS = 1000
 
 def main(args):
     global es, api
@@ -29,19 +32,23 @@ def main(args):
             pb.ETA(), " "
         ]
     bar = pb.ProgressBar(max_value=len(users), widgets=widgets)
+    i = 0
     for username in bar(users):
+        if i > 100:
+            break
         widgets[6] = pb.FormatLabel("User: " + username + " ")
         find_twins(users[username], args.user_index)
-        
-        with open("find_twins.log", "a") as f:
-            f.write(username + ": " + str(len(users[username]["possible_twins"])) + "\n")
-        
-    
-    f.close()
-       
-
+        i +=1
+               
     with open("users_and_possible_twins.pickle", "wb") as f:
         pickle.dump(users, f)
+
+    set_users = set()
+    for user in users:
+        for u in users[user]["possible_twins"]:
+            set_users.add(u["name"])
+    
+    print(len(set_users))
 
     # Filtrar por número de posts
 
@@ -76,62 +83,129 @@ def get_users(index):
 
 def find_twins(user, index):
     MONTH_IN_SECONDS = 30*24*3600 
+
     bounds = {
-        "created_old": user["created_utc"] - MONTH_IN_SECONDS,
-        "created_new": user["created_utc"] + MONTH_IN_SECONDS,
+        "intervals": get_time_intervals(user["created_utc"]),
         "comment_low": user["comment_karma"] - user["comment_karma"]*0.1,
         "comment_high": user["comment_karma"] + user["comment_karma"]*0.1,
         "link_low": user["link_karma"] - user["link_karma"]*0.1,
         "link_high": user["link_karma"] + user["link_karma"]*0.1
     }
 
-    pp.pprint(bounds)
-    res = es.search(index = index,
-        body = {
-            "size": MAX_USERS,
-            "query":{
-                "bool":{
-                    "must":[
-                        {
-                            "range":{
-                                "created_utc":{
-                                    "gte": bounds["created_old"],
-                                    "lte": bounds["created_new"]
+    num_hits = 0
+    i = 0
+    while num_hits < MAX_USERS and i < len(bounds["intervals"]):
+        res = es.search(index = index,
+            body = {
+                "size": MAX_USERS,
+                "query":{
+                    "bool":{
+                        "must":[
+                            {
+                                "range":{
+                                    "created_utc":{
+                                        "gte": bounds["intervals"][i]["date_old"],
+                                        "lte": bounds["intervals"][i]["date_new"]
+                                    }
+                                }
+                            },
+                            {
+                                "range":{
+                                    "comment_karma":{
+                                        "gte": bounds["comment_low"],
+                                        "lte": bounds["comment_high"]
+                                    }
+                                }
+                            },
+                            {
+                                "range":{
+                                    "link_karma":{
+                                        "gte": bounds["link_low"],
+                                        "lte": bounds["link_high"]
+                                    }
                                 }
                             }
-                        },
-                        {
-                            "range":{
-                                "comment_karma":{
-                                    "gte": bounds["comment_low"],
-                                    "lte": bounds["comment_high"]
-                                }
-                            }
-                        },
-                        {
-                            "range":{
-                                "link_karma":{
-                                    "gte": bounds["link_low"],
-                                    "lte": bounds["link_high"]
-                                }
-                            }
-                        }
-                    ]
+                        ]
+                    }
+
                 }
-
             }
-        }
-    )
+        )
+        num_hits = res["hits"]["total"]["value"]
+        hits = res["hits"]["hits"]
+        i+=1
 
-    bar = pb.ProgressBar()
-    user["possible_twins"] = [hit["_source"] for hit in bar(res)]
-
+    user["possible_twins"] = [hit["_source"] for hit in hits]
+            
 def get_time_intervals(timestamp):
-    intervals = []
+    """
+        Devuelve una lista de fechas en timestamp con los límites de los siguientes intervalos:
+            * +/- 12 horas en el mismo día
+            * +/- 24 horas
+            * +/- 3.5 días en la misma semana
+            * +/- 3 días
+            * +/- 15 días en el mismo mes
+            * +/- 30 días
+
+        Parámetros
+        ----------
+        timestamp: int
+            \t Timestamp de la fecha entorno a la cuál se calculan los intervalos
+
+        Salida
+        ------
+        list
+            \tLista con los timestamps correspondientes a los límites de los rangos
+    """
+    bounds = []
+    
     date = dt.fromtimestamp(timestamp)
+
+    # Intervalo 12 horas pero dentro del mismo día
+    beginning_day = date.replace(hour=00, minute=00, second=00)
+    end_day = date.replace(hour=23, minute=59, second=59)
+
+    half_day_beginning = max(beginning_day, date - timedelta(hours=12)).timestamp()
+    half_day_end = min(end_day, date + timedelta(hours=12)).timestamp()
+    bounds.append({"date_old": half_day_beginning, "date_new": half_day_end })
+
+    # Intervalo 24 horas
+    bounds.append({"date_old": (date - timedelta(hours=24)).timestamp(),
+        "date_new": (date + timedelta(hours=24)).timestamp()})
+
+    # Intervalo 3.5 días pero en la misma semana
+    ## Se calcula el primer día de la semana
+    beginning_week = date - timedelta(days=date.weekday())
+    beginning_week = beginning_week.replace(hour=00, minute=00, second=00)
+    ## Se calcula el último
+    end_week = beginning_week + timedelta(days=6)
+    end_week = end_week.replace(hour=23, minute=59, second=59)
+    ## Se calculan el rango de 3.5 días asegurándose de no salir de la semana
+    half_week_beginning = max(beginning_week, date - timedelta(days=3.5)).timestamp()
+    half_week_end = min(end_week, date + timedelta(days=3.5)).timestamp()
+    bounds.append({"date_old": half_week_beginning, "date_new": half_week_end})
+
+    # Intervalo 3 días
+    bounds.append({"date_old": (date - timedelta(days=3)).timestamp(),
+        "date_new": (date + timedelta(days=3)).timestamp()})
+
+    # Intervalo 15 días pero dentro del mismo mes
+    beginning_month = date.replace(day=1, hour=00, minute=00, second=00)
+    ## Se saca el último día del mes
+    next_month = date.replace(day=28) + timedelta(days=4)
+    end_month = next_month - timedelta(days=next_month.day)
+    end_month = end_month.replace(hour=23, minute=59, second=59)
+    ## Se calcula el rango de 15 días sin salirse del mes
+    half_month_beginning = max(beginning_month, date - timedelta(days=15)).timestamp()
+    half_month_end = min(end_month, date + timedelta(days=15)).timestamp()
+    bounds.append({"date_old": half_month_beginning, "date_new": half_month_end})
+
+    # Intervalo 30 días
+    bounds.append({"date_old": (date - timedelta(days=30)).timestamp(),
+        "date_new": (date + timedelta(days=30)).timestamp()})
     
 
-    return intervals
+    return bounds
 
 def parse_args():
     """
